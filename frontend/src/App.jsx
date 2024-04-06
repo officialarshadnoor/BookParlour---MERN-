192e2082fe9e25b2f82fa45ba856ae84951ce76e
blogginghindi@gmail.com <arshadnoor585@gmail.com>
2024-04-06 23:50:35 +0530

    frontend some more functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Home from './home/Home';
 import {Route, Routes} from 'react-router-dom';
 import Courses from './courses/Courses';
+import Signup from './components/Signup';
+import ContactP from './contact/ContactP';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/course" element={<Courses/>}/>
+    <Route path="/contact" element={<ContactP/>}/>
+    <Route path="/signup" element={<Signup/>}/>
     </Routes>
     </div>
     </>
